19e85b0ef34e7dbda4556def644e003b6d1a3ea2
SAJAN  AADE <143798451+git-saju@users.noreply.github.com>
2026-03-14 13:02:48 +0530

    NumberPattern1.java
    Refactored NumberPattern1.java to improve readability and structure. Removed unnecessary variables and streamlined the main method.

--- NumberPattern1.java
+++ NumberPattern1.java
@@ -1,26 +1,21 @@
-import java.util.scanner;
+import java.util.Scanner;
 
-public class NumberPattern1{
+public class NumberPattern {
 
-public static void main (String []args){
+    public static void main(String[] args) {
 
-  Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-  int rows = sc.nextInt();
+        int number = 1;
 
-  int number= 1;
+        for (int i = 1; i <= 5; i++) {          // rows
+            for (int j = 1; j <= i; j++) {      // columns
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
 
-  for(int i = 1; i<=rows; i++){
-
-    for(int j = 1; j<=i; j++){
-
-      System.out.print(number + "");
-      number++;
+        sc.close();
     }
-    System.out.println();
-   }
-  sc.close();
-  }
-  
 }
-  
