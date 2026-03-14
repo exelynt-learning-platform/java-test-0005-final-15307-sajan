d8ed1c68c78ff65453ce0ff13d12037041188cdb
SAJAN  AADE <143798451+git-saju@users.noreply.github.com>
2026-03-14 14:58:45 +0530

    print number pattern

---
+++ NumberPattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NumberPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = 1;
+
+        for (int i = 1; i <= 5; i++) {          
+            for (int j = 1; j <= i; j++) {      
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
